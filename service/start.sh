f49ef017eb93326ccd97353a06aeb2497975caa7
GeoJosh <joshua.penton@geocent.com>
2020-04-07 23:14:32 -0500

    Utilize supervisord to monitor gunicorn process

--- service/start.sh
+++ service/start.sh
@@ -2,4 +2,5 @@
 
 flask init-db
 
-/usr/local/bin/gunicorn --workers 2 --bind :8000 "app:create_app()"
+# /usr/local/bin/gunicorn --workers 2 --bind :8000 "app:create_app()"
+supervisord -n -c /opt/supervisor/supervisord.conf 
